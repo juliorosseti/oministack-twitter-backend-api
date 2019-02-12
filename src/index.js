096dc362559f159cb829e587c2136471aec08c59
Júlio Rosseti <juliorosseti@gmail.com>
2019-02-12 18:48:12 -0200

    Separado arquivo de rotas + criação do metodo store/index para os tweets

--- src/index.js
+++ src/index.js
@@ -10,9 +10,7 @@ mongoose.connect(
     }
 );
 
-app.get('/', (req, res) => {
-    return res.send('Hello Word!');
-});
+app.use(require('./routes'));
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
