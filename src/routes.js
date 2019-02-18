f2844b4bc02b5db43e8fb8b98cb5209592133f89
Júlio Rosseti <juliorosseti@gmail.com>
2019-02-18 13:21:33 -0300

    Adicionado novo model de usuários e rota para registro

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,13 @@ const routes = express.Router();
 
 const TweetController = require('./controllers/TweetController');
 const LikeController = require('./controllers/LikeController');
+const AuthController = require('./controllers/AuthController');
 
 routes.get('/tweets', TweetController.index);
 routes.post('/tweets', TweetController.store);
 routes.delete('/tweets/:id', TweetController.delete);
 routes.post('/likes/:id', LikeController.store);
+
+routes.post('/register', AuthController.store);
 
 module.exports = routes;
